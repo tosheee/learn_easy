8a07cab1d6c7db85eb81e4a21f5e8e04864220f5
Todor Chakarov <tosheee@abv.bg>
2017-01-09 00:26:23 +0200

    Removed unnecessary code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,32 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        check_text()
+    }
+});
+
+function view_translations() {
+    document.getElementById('view-text-hidden').style.visibility = 'visible';
+}
+
+function check_text() {
+    var hidden_word  = document.getElementById('view-text-hidden').innerHTML.replace(/\s/g,'');
+    var text_message = document.getElementById('text-message');
+    var enter_word   = document.getElementById('enter-text');
+    if (hidden_word == enter_word.value) {
+
+        text_message.innerHTML = "Exelent";
+
+        location.reload();
+    } else {
+        text_message.innerHTML = "Check what is written";
+        enter_word.value = '';
+        view_translations()
+    }
+}
+
+
